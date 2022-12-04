cc582634809a27ada8b34e8491469fd4b95f6584
Donovan <donovangriffin04@gmail.com>
2022-12-03 18:05:19 -0800

    comments

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+
+//asks user permission to use tracking
 const locationBtn = document.querySelector('.allow')
 locationBtn.addEventListener('click', latAndLong)
 
-
-    let lat = '';
-    let long = '';
+//function to get latitude and longitude
+//sets global variables to be updated
+let lat = '';
+let long = '';
 function latAndLong(){
   navigator.geolocation.getCurrentPosition((position) => {
     lat = position.coords.latitude;
@@ -13,16 +16,18 @@ function latAndLong(){
   )
 }
 
+//axios request to get weather
 const weatherDisplay = document.querySelector('.weatherLive')
 async function weatherCall() {
   const weather = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=rain,snowfall&current_weather=true`)
 console.log(weather)
-weatherDisplay.innerText = `Temperature: ${weather.data.current_weather.temperature} C`
-document.querySelector('.snow').innerText = `Snow: ${weather.data.hourly.snowfall[0]} CM`
-document.querySelector('.rain').innerText = `Rain: ${weather.data.hourly.rain[0]} MM`
+//adds weather to page
+  weatherDisplay.innerText = `Temperature: ${weather.data.current_weather.temperature} C`
+  document.querySelector('.snow').innerText = `Snow: ${weather.data.hourly.snowfall[0]} CM`
+  document.querySelector('.rain').innerText = `Rain: ${weather.data.hourly.rain[0]} MM`
 }
 
 
-
+//initializes weatherCall
 const what = document.querySelector('.whats')
 what.addEventListener('click', weatherCall)
