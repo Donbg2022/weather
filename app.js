b18642ccb41fa0298a00e487579f4c1631379728
Donovan <donovangriffin04@gmail.com>
2022-12-03 18:01:43 -0800

    addded snow and rain

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ function latAndLong(){
   )
 }
 
-
+const weatherDisplay = document.querySelector('.weatherLive')
 async function weatherCall() {
   const weather = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=rain,snowfall&current_weather=true`)
 console.log(weather)
+weatherDisplay.innerText = `Temperature: ${weather.data.current_weather.temperature} C`
+document.querySelector('.snow').innerText = `Snow: ${weather.data.hourly.snowfall[0]} CM`
+document.querySelector('.rain').innerText = `Rain: ${weather.data.hourly.rain[0]} MM`
 }
 
 
